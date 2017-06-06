7a6d7ffc0184905795256f0530c6b5d804916014
ashok kumar <kumar.ak.ashok@gmail.com>
2017-06-06 20:51:12 +0530

    Problem 2 R script

---
+++ plot2.R
@@ -0,0 +1,17 @@
+        ## Read data from data file 
+        NEI <- readRDS("./data/summarySCC_PM25.rds")
+
+        ## Read the classification file        
+
+        SCC <- readRDS("./data/Source_Classification_Code.rds")
+        
+        marylanddata <- subset(NEI, NEI$fips == "24510")
+
+        sumforyear <- aggregate(Emissions ~ year, marylanddata, sum)
+
+        png('plot2.png')
+
+        barplot(height=sumforyear$Emissions, names.arg=sumforyear$year, xlab="years", ylab=expression('total PM'[2.5]*' emission'),main=expression('Total PM'[2.5]*' emissions at various years'))
+
+        dev.off()
+        
